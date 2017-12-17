083cdc0bdf54659ff39e3a68ad8ee738b7f91874
youchao.liu <liuyouchao111@gmail.com>
2017-12-17 09:16:01 +0800

    just have runnable version

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,9 +1,10 @@
-import DeferMap from '../src';
-// const DeferMap = require('../src');
+import DeferMap from "../src";
+// const DeferMap = require('../src').default;
 
-test('basic', function() {
+test("test", (callback) => {
   let start = +new Date();
 
+  // expect(1).toEqual(1);
   const df = new DeferMap([1, 2, 3], (i, key) => {
     const exec = +new Date();
     return i;
@@ -11,6 +12,6 @@ test('basic', function() {
 
   return df.then((res) => {
     expect(res).toEqual([1, 2, 3]);
+    callback();
   })
 })
-
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,49 @@
 module.exports = {
-  verbose: true,
-  collectCoverage: false,
-  collectCoverageFrom: [
-    "src/*.{js,jsx}",
-    "!**/node_modules/**",
-    "!**/vendor/**"
-  ],
+  "moduleFileExtensions": ["ts", "tsx", "js"],
+  "globals": {
+    "ts-jest": {
+      // "babelConfig": {
+      //   // "presets": ["env"],
+      //   // "plugins": ["transform-es2015-modules-commonjs"]
+
+      //   "presets": [["es2015", {"modules": false}]],
+      //   "env": {
+      //     "test": {
+      //       "presets": [["es2015"]]
+      //     }
+      //   }
+      // },
+      "useBabelrc": true
+    }
+  },
+  "transform": {
+    // "^.+\\.jsx?$": "ts-jest",
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest",
+    // "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  // "testMatch": ["**/__tests__/*.(ts|tsx|js)"],
+  // verbose: true,
+  // collectCoverage: false,
+  // collectCoverageFrom: [
+  //   "src/*.{js,jsx}",
+  //   "!**/node_modules/**",
+  //   "!**/vendor/**"
+  // ],
 };
+
+// "jest": {
+//   "moduleDirectories": ["node_modules", "src"],
+//   "moduleFileExtensions": [
+//     "ts",
+//     "tsx",
+//     "js"
+//   ],
+//   "transform": {
+//     "^.+\\.(ts|tsx)$": "<rootDir>/preprocessor.js"
+//   },
+//   "testMatch": [
+//     "**/__tests__/*.(ts|tsx|js)"
+//   ]
+// },
